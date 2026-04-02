28e5c5c30d8434b688002e12fd69ecb2cb79d64d
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2026-04-02 19:41:33 +0200

    Refactor Footer component structure to render the ThemeSelector component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,6 +5,7 @@ import LogoIcon from "/assets/images/logo.svg"
 import { cn } from "@/lib/utils"
 import { ShortcutLegend } from "@/home/main/shortcuts.kbd"
 import { useEngineConfig } from "@/home/context/engine.context"
+import { ThemeSelector } from "../theme-selector"
 
 export const Footer = () => {
   const { isImmersive } = useEngineConfig()
@@ -12,22 +13,25 @@ export const Footer = () => {
   return (
     <footer
       aria-hidden={isImmersive}
-      className={cn("mt-auto grid gap-6", isImmersive && "immersive-mode")}>
+      className={cn("mt-auto grid gap-4", isImmersive && "immersive-mode")}>
       <ShortcutLegend />
 
-      <p className="text-muted-foreground/30 dark:text-muted/50 text-6 flex items-center justify-center gap-1">
-        <span>© {new Date().getFullYear()}</span>
-        <span className="flex items-center gap-1">
-          <Image
-            src={LogoIcon}
-            alt="Logo"
-            width={20}
-            height={20}
-          />
-          TST
-        </span>
-        <span>. All rights reserved.</span>
-      </p>
+      <div className="flex items-center justify-between gap-2">
+        <p className="text-muted-foreground/50 dark:text-muted/50 text-6 flex items-center justify-center gap-1">
+          <span>© {new Date().getFullYear()}</span>
+          <span className="flex items-center gap-1">
+            <Image
+              src={LogoIcon}
+              alt="Logo"
+              width={20}
+              height={20}
+            />
+            TST
+          </span>
+          <span>. All rights reserved.</span>
+        </p>
+        <ThemeSelector />
+      </div>
     </footer>
   )
 }
